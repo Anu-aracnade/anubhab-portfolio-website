01a284b76859e10610f754a2095a2a257cdd99ec
Anubhab Pathak <anubhab01.aracnade@gmail.com>
2026-06-29 10:25:23 +0530

    feat:Add Smooth-SlideUp-Scroll-Effect to each sections and particular elements

--- script.js
+++ script.js
@@ -23,3 +23,24 @@ hamburger.addEventListener("click", toggleMenu);
 navLinks.forEach(link => {
   link.addEventListener("click", closeMenu);
 });
+
+// ----------------------Smooth-SlideUp-Scroll-Effect-------------------------
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // Check if the element is visible on the screen
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      
+      //Stop observing as I only want the animation to play ONCE
+      observer.unobserve(entry.target); 
+    }
+  });
+}, {
+  threshold: 0.15 // Triggers when 15% of the element is visible
+});
+
+// Grab all hidden elements and attach the observer
+const hiddenElements = document.querySelectorAll('.hidden-element');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
